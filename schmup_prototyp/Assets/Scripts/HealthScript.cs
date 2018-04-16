74e10883c048002e307d9399a6a21c571ebf10b4
Valentin Bach <valentin691@hotmail.de>
2018-04-16 10:12:39 +0200

    -Stand vom Pitch. Versuch Nr. 2

---
+++ schmup_prototyp/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+
+    public int hp = 2;
+
+    public bool isEnemy = true;
+
+
+    public Sprite sprite1; // Drag your first sprite here
+    public Sprite sprite2; // Drag your second sprite here
+    private SpriteRenderer spriteRenderer;
+
+    float delay = 0.32f; 
+
+
+    public BoxCollider2D EnemyBox;
+
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
+
+
+
+        if (shot != null)
+        {
+            if (shot.isEnemyShot != isEnemy)
+            {
+                hp -= shot.damage;
+
+                Destroy(shot.gameObject);
+
+                if(hp <= 0)
+                {
+                    Destroy(EnemyBox);
+                    ChangeTheDamnSprite(); // call method to change sprite
+                    Destroy(gameObject, delay);
+                }
+            }
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
+        if (spriteRenderer.sprite == null) // if the sprite on spriteRenderer is null then
+            spriteRenderer.sprite = sprite1; // set the sprite to sprite1
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+
+
+    void ChangeTheDamnSprite()
+    {
+        if (spriteRenderer.sprite == sprite1) // if the spriteRenderer sprite = sprite1 then change to sprite2
+        {
+            spriteRenderer.sprite = sprite2;
+        }
+        else
+        {
+            spriteRenderer.sprite = sprite1; // otherwise change it back to sprite1
+        }
+    }
+}

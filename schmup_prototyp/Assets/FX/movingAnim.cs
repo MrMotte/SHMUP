e04f1fae70e0dc651073577fa2df0879c6829b87
Unknown <christian-schildhauer@web.de>
2018-06-22 18:37:01 +0200

    Effects / Anims / RP
    - added LW RP
    - added Animations (Bomber, Chaser, BigEye)
    - added Hit FX, SmokeAndStars FX, BasicLaser FX

---
+++ schmup_prototyp/Assets/FX/movingAnim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingAnim : MonoBehaviour {
+
+
+	public float playDuration = 1f;
+	public float degreePerSecond = 90f;
+	public Quaternion localRot;
+
+
+	Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		
+		rb2d = this.GetComponent<Rigidbody2D> ();	
+		localRot = this.transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (Input.GetMouseButtonDown (0)) {
+			//RotateSprite ();
+			Debug.Log("MouseButton");
+			localRot.z += 1f;
+			transform.localRotation = localRot;
+		}
+		if (Input.GetButtonUp ("Fire1")) {
+			localRot = transform.localRotation;
+		}
+
+	}
+}

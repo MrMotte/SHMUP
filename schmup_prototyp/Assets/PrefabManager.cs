81282d0fe2662fa1dcb6e2f793b5f54d7a4f4728
Bernd Gaykowski <b.gaykowski@googlemail.com>
2018-07-02 09:58:17 +0200

    Prefab Manager - first try

---
+++ schmup_prototyp/Assets/PrefabManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+
+[System.Serializable]
+public struct Enemies
+{
+	public GameObject Enemy;
+	public List<Component> EnemyScriptsList;
+}
+
+
+[InitializeOnLoad]
+public class PrefabManager : MonoBehaviour {
+
+
+	public Enemies[] EnemyArray;
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnValidate()
+	{
+		for(int i = 0; i < EnemyArray.Length; i++)
+		{
+			EnemyArray[i].Enemy.gameObject.GetComponents(typeof(MonoBehaviour), EnemyArray[i].EnemyScriptsList);
+		}
+	}
+}

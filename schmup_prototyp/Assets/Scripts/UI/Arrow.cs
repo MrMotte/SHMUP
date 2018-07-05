c8eb7c15266401b7f92f3780eb7c8cd80be9f3c2
valentin bach <valentin691@hotmail.de>
2018-07-05 21:56:02 +0200

    Added Arrow script

--- schmup_prototyp/Assets/Scripts/UI/Arrow.cs
+++ schmup_prototyp/Assets/Scripts/UI/Arrow.cs
@@ -17,7 +17,7 @@ public class Arrow : MonoBehaviour {
 	{
 		if (player.transform.position.y >= player.GetComponent<PlayerScript>().Y_WaterBorder)
 		{
-			if (player.GetComponent<PlayerScript>().CurrentWeapon == 1)
+			if (player.GetComponent<PlayerScript>().CurrentWeapon == 0)
 			{
 				transform.localPosition = new Vector3(-370, -165, 0);
 				transform.localRotation = Quaternion.Euler(0, 0, 0);
@@ -27,7 +27,7 @@ public class Arrow : MonoBehaviour {
 				transform.localPosition = new Vector3(-342, -167, 0);
 				transform.localRotation = Quaternion.Euler(0, 0, 315);
 			}
-			else if (player.GetComponent<PlayerScript>().CurrentWeapon == 3)
+			else if (player.GetComponent<PlayerScript>().CurrentWeapon == 4)
 			{
 				transform.localPosition = new Vector3(-340, -195, 0);
 				transform.localRotation = Quaternion.Euler(0, 0, 270);
@@ -40,12 +40,12 @@ public class Arrow : MonoBehaviour {
 				transform.localPosition = new Vector3(-340, 195, 0);
 				transform.localRotation = Quaternion.Euler(0, 0, 270);
 			}
-			else if (player.GetComponent<PlayerScript>().CurrentWeapon == 2)
+			else if (player.GetComponent<PlayerScript>().CurrentWeapon == 3)
 			{
 				transform.localPosition = new Vector3(-342, 167, 0);
 				transform.localRotation = Quaternion.Euler(0, 0, 225);
 			}
-			else if (player.GetComponent<PlayerScript>().CurrentWeapon == 3)
+			else if (player.GetComponent<PlayerScript>().CurrentWeapon == 5)
 			{
 				transform.localPosition = new Vector3(-370, 165, 0);
 				transform.localRotation = Quaternion.Euler(0, 0, 180);

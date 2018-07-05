fb874386698801a62c6a083a152664908ced4b46
Bernd Gaykowski <b.gaykowski@googlemail.com>
2018-07-05 02:41:16 +0200

    UI Equipment Screen

---
+++ schmup_prototyp/Assets/VariableSaver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableSaver : MonoBehaviour {
+
+    private static bool created = false;
+	//private static 
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+}

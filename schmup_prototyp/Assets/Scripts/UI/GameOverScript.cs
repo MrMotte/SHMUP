a0e74eca91aa45b6d2b33f271ff1b910f18e67f5
Unknown <franziskaklien@yahoo.de>
2018-07-11 18:24:15 +0200

    Renamed Files

---
+++ schmup_prototyp/Assets/Scripts/UI/GameOverScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+
+    private GUISkin skin;
+
+    void Start()
+    {
+        skin = Resources.Load("GUISkin") as GUISkin;
+    }
+
+    public void OnGUI()
+    {
+        const int buttonWidth = 120;
+        const int buttonHeight = 60;
+
+        GUI.skin = skin;
+
+        if (
+            GUI.Button(
+
+                new Rect(
+                Screen.width / 2 - (buttonWidth / 2),
+                (1 * Screen.height / 3) - (buttonHeight / 2),
+                buttonWidth,
+                buttonHeight
+                ),
+                "Retry!"
+                )
+            )
+        {
+            SceneManager.LoadScene("1_Level");
+
+        }
+
+
+        if (
+            GUI.Button(
+
+                new Rect(
+                Screen.width / 2 - (buttonWidth / 2),
+                (2 * Screen.height / 3) - (buttonHeight / 2),
+                buttonWidth,
+                buttonHeight
+                ),
+                "Menu"
+                )
+            )
+            {
+                SceneManager.LoadScene("Menu");
+            }
+    }
+}
---
+++ schmup_prototyp/Assets/Scripts/UI/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    private GUISkin skin;
+
+    void Start()
+    {
+        skin = Resources.Load("GUISkin") as GUISkin;
+    }
+
+    public void OnGUI()
+    {
+        const int buttonWidth = 84;
+        const int buttonHeight = 60;
+
+        GUI.skin = skin;
+
+        if (
+            GUI.Button(
+                
+                new Rect(
+                Screen.width / 2 - (buttonWidth / 2 ),
+                (1 * Screen.height / 2) - (buttonHeight / 2 ),
+                buttonWidth,
+                buttonHeight
+                ),
+                "Start!"
+                )
+            )
+        {
+            SceneManager.LoadScene("1_Level");
+        }
+
+    }
+
+}

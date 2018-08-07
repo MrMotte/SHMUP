031e4618b63d29418fe5f281e38a657559ee6926
Bernd Gaykowski <b.gaykowski@googlemail.com>
2018-08-07 15:37:36 +0200

    Cannonball & Swarm & Bomber & Shotgun

--- schmup_prototyp/Assets/Scripts/Weapons/WeaponScript.cs
+++ schmup_prototyp/Assets/Scripts/Weapons/WeaponScript.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponScript : MonoBehaviour {
+public class WeaponScript : MonoBehaviour
+{
 
     //--------------------------------
     // 1 - Designer variables
@@ -17,11 +18,19 @@ public class WeaponScript : MonoBehaviour {
     /// Cooldown in seconds between two shots
     /// </summary>
     public float shootingRate = 0.25f;
-	public bool randomRange =false;
-	public float rangeMin;
-	public float rangeMax;
+    public bool randomRange = false;
+    public float rangeMin;
+    public float rangeMax;
+
+    public bool shootVolley;
+    public int volleyAmount;
+    public float volleyTimeDif;
+	private int volleyCounter;
+
+	private bool mIsEnemy;
 
     public AudioSource ShootSound;
+	public Animator enemyAnimator;
 
     //--------------------------------
     // 2 - Cooldown
@@ -32,6 +41,7 @@ public class WeaponScript : MonoBehaviour {
     void Start()
     {
         shootCooldown = 0f;
+
     }
 
     void Update()
@@ -41,64 +51,35 @@ public class WeaponScript : MonoBehaviour {
             shootCooldown -= Time.deltaTime;
         }
 
-		if(Time.frameCount % 10 == 0 && randomRange)
-		{
+        if (randomRange)
+        {
 
-		Quaternion target = Quaternion.Euler(0, 0, Random.Range(rangeMin,rangeMax));
-			this.transform.rotation = target;
-		}
+            Quaternion target = Quaternion.Euler(0, 0, Random.Range(rangeMin, rangeMax));
+            this.transform.rotation = target;
+        }
     }
 
-	//--------------------------------
-	// 3 - Shooting from another script
-	//--------------------------------
+    //--------------------------------
+    // 3 - Shooting from another script
+    //--------------------------------
 
-	/// <summary>
-	/// Create a new projectile if possible
-	/// </summary>
-	public void Attack(bool isEnemy)
-	{
-		if (CanAttack)
-		{
-			shootCooldown = shootingRate;
-			// Create a new shot
-
-				var shotTransform = Instantiate(shotPrefab) as Transform;
-
-			if (ShootSound != null)
-				ShootSound.Play();
-
-
-			// Assign position
-			if (isEnemy)
+    /// <summary>
+    /// Create a new projectile if possible
+    /// </summary>
+    public void Attack(bool isEnemy)
+    {
+        if (CanAttack)
+        {
+			//mIsEnemy = isEnemy;
+            StartCoroutine("ShootBullet", isEnemy);
+			if(gameObject.tag == "Bomber" && enemyAnimator != null)
 			{
-				Vector3 bulletOffset = new Vector3(-1, 0, 0);
-				shotTransform.position = transform.position + bulletOffset;
+				enemyAnimator.SetTrigger("BomberAttack");
+				
+				Debug.Log("AnimationFired!");
 			}
-			else
-			{
-				Vector3 bulletOffset = new Vector3(1.5f, 0, 0);
-				shotTransform.position = transform.position + bulletOffset;
-				if(randomRange)
-					shotTransform.rotation = shotTransform.rotation * Quaternion.Euler(0,0,Random.Range(-100,100));
-			}
-
-
-			// The is enemy property
-			ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
-			if (shot != null)
-			{
-				shot.isEnemyShot = isEnemy;
-			}
-
-			// Make the weapon shot always towards it
-			MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
-			if (move != null)
-			{
-				move.direction = this.transform.right; // towards in 2D space is the right of the sprite
-			}
-		}
-	}
+        }
+    }
 
     /// <summary>
     /// Is the weapon ready to create a new projectile?
@@ -109,5 +90,53 @@ public class WeaponScript : MonoBehaviour {
         {
             return shootCooldown <= 0f;
         }
+    }
+
+    IEnumerator ShootBullet(bool isEnemy)
+    {
+		volleyCounter = volleyAmount;
+		do{
+			
+        shootCooldown = shootingRate;
+        // Create a new shot
+
+        var shotTransform = Instantiate(shotPrefab) as Transform;
+
+        if (ShootSound != null)
+            ShootSound.Play();
+
+
+        // Assign position
+        if (isEnemy)
+        {
+            Vector3 bulletOffset = new Vector3(-1, 0, 0);
+            shotTransform.position = transform.position + bulletOffset;
+        }
+        else
+        {
+            Vector3 bulletOffset = new Vector3(1.5f, 0, 0);
+            shotTransform.position = transform.position + bulletOffset;
+            if (randomRange)
+                shotTransform.rotation = shotTransform.rotation * Quaternion.Euler(0, 0, Random.Range(-100, 100));
+        }
+
+
+        // The is enemy property
+        ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+        if (shot != null)
+        {
+            shot.isEnemyShot = isEnemy;
+        }
+
+        // Make the weapon shot always towards it
+        MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+        if (move != null)
+        {
+            move.direction = this.transform.right; // towards in 2D space is the right of the sprite
+        }
+
+		volleyCounter--;
+		yield return new WaitForSeconds(volleyTimeDif);
+	}while(shootVolley && volleyCounter > 0);
     }
 }

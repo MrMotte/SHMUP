687354994dd4c980359b74b30b8cacf2e1ad32fc
valentin bach <valentin691@hotmail.de>
2018-08-08 21:17:30 +0200

    Update

--- schmup_prototyp/Assets/Healthbar.cs
+++ schmup_prototyp/Assets/Healthbar.cs
@@ -27,16 +27,20 @@ public class Healthbar : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (( player.GetComponent<HealthScript>().hp >= 12) && (player.GetComponent<HealthScript>().hp <= 17)){
+		// FRANZI HIER DIE HP WERTE VERÄNDERN. Das unten ist mit 20 HP. Jetzt musst du einfach nur 4/5 3/5 2/5 1/5 nehmen. ( 4/5 = 16 ) ( 3/5 = 12) etc.
+		// FRANZI HIER DIE HP WERTE VERÄNDERN. Das unten ist mit 20 HP. Jetzt musst du einfach nur 4/5 3/5 2/5 1/5 nehmen. ( 4/5 = 16 ) ( 3/5 = 12) etc.
+		// FRANZI HIER DIE HP WERTE VERÄNDERN. Das unten ist mit 20 HP. Jetzt musst du einfach nur 4/5 3/5 2/5 1/5 nehmen. ( 4/5 = 16 ) ( 3/5 = 12) etc.
+
+		if (( player.GetComponent<HealthScript>().hp >= 13) && (player.GetComponent<HealthScript>().hp <= 16)){
 			Anzeige.sprite = Healthbar_80;
-		}else
-		if (( player.GetComponent<HealthScript>().hp >= 8) && (player.GetComponent<HealthScript>().hp <= 13)){
+		}
+		if (( player.GetComponent<HealthScript>().hp >= 9) && (player.GetComponent<HealthScript>().hp <= 12)){
 			Anzeige.sprite = Healthbar_60;
-		}else
-		if (( player.GetComponent<HealthScript>().hp >= 4) && (player.GetComponent<HealthScript>().hp <= 9)){
+		}
+		if (( player.GetComponent<HealthScript>().hp >= 4) && (player.GetComponent<HealthScript>().hp <= 8)){
 			Anzeige.sprite = Healthbar_40;
-		}else
-		if (( player.GetComponent<HealthScript>().hp >= 0) && (player.GetComponent<HealthScript>().hp <= 5)){
+		}
+		if (player.GetComponent<HealthScript>().hp <= 3){
 			Anzeige.sprite = Healthbar_20;
 		}
 	}
